6b7ee9834a0cacc8b64361ef4baea17f2dc8adaf
Whesley Xavier <xavierwhesley@gmail.com>
2024-09-25 15:43:09 -0300

    wip: Drone and Server

--- ComunicaoIndireta/src/Drone.java
+++ ComunicaoIndireta/src/Drone.java
@@ -1,9 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.Socket;
+
 public class Drone {
     //Precisa de um Socket que envia os dados coletados ao servidor, que ainda nao sei direito como sera implementado.
     private double AtmosphericPressure; //Pressão Atmosferica
     private double Radiation; //Radiação Solar
     private double Temperature; //Temperatura
     private double Moisture; //Umidade
+    private int id; //Id do drone
+    private String local;
+    private MulticastSocket multicastSocket = null;
+    private InetAddress multicastIp = null;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Construtores
@@ -16,9 +28,38 @@ public class Drone {
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //Envio de dados usando comunicação entre servidores
+    //Main
 
+    public static void main(String[] args) {
 
+        try(Socket socket =
+                    new Socket("localhost", 12345);
+            PrintWriter saida =
+                    new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader entrada = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            BufferedReader console = new BufferedReader(
+                    new InputStreamReader(System.in))) {
+
+            System.out.println("Conectador ao servidor: " +
+                    entrada.readLine());
+
+            String msg;
+            while((msg = console.readLine()) != null){
+                saida.println(msg);
+                //System.out.println("Mensagem do servidor " + msg);
+            }
+
+        }catch (IOException e){
+            e.getMessage();
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Coleta de dados
+    private double calcTemperature(){
+        return 10 + (Math.random() * 40);
+    }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Getters e Setters
@@ -52,5 +93,21 @@ public class Drone {
 
     public void setMoisture(double moisture) {
         Moisture = moisture;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
     }
 }
