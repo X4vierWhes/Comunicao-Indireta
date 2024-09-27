3f0190a11b008d9fdccf4c74e0a0868a637fdff6
João Lucas <joao.duarte31219@alunos.ufersa.edu.br>
2024-09-27 03:35:48 -0300

    add: funções de servidor

--- ComunicaoIndireta/src/Server.java
+++ ComunicaoIndireta/src/Server.java
@@ -2,12 +2,63 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+// TODO: adicionar log de servidor
+// TODO: enviar dados para endereço de multicast
+
 public class Server {
+    private DatagramSocket droneCon;
+    private MulticastSocket group;
+    private ExecutorService pool;
+
+    private InetAddress clientAddr;
+
+    public Server(InetAddress addr, InetAddress groupAddr, int port) {
+        try {
+            droneCon = new DatagramSocket(port);
+
+            group = new MulticastSocket(port);
+            InetSocketAddress grupo = new InetSocketAddress(groupAddr, port);
+            NetworkInterface interfaceRede = NetworkInterface.getByName("en0");
+            group.joinGroup(grupo, interfaceRede);
+
+            pool = Executors.newCachedThreadPool();
+
+            pool.execute(this::handleDroneConnection);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+private void handleDroneConnection() {
+    DatagramPacket dp = new DatagramPacket(new byte[1024], 1024);
+
+    while (true) {
+        try {
+            droneCon.receive(dp);
+
+            // guardar num log
+
+            // reenviar diretamente para próximo grupo
+            pool.submit(() -> {
+                try {
+                    dp.setAddress(clientAddr);
+                    group.send(dp);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            System.out.println("Recebi: " + new String(dp.getData()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
     public static void main(String[] args) {
         ExecutorService pool = Executors.newFixedThreadPool(10);
 
